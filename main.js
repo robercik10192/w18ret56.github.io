f7b7338914d77c900f6705077d26a0761220a128
w18ret56 <54064436+w18ret56@users.noreply.github.com>
2019-08-24 11:21:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var script = document.createElement("script");
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/aes-js/3.1.2/index.min.js";
-document.body.appendChild(script);
-console.log(aesjs);
+$(document).load(function(){
+  var script = document.createElement("script");
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/aes-js/3.1.2/index.min.js";
+  document.body.appendChild(script);
+  console.log(aesjs);
+});
