67f69d969c9cf38624a5635dd4988278556f8dcf
w18ret56 <54064436+w18ret56@users.noreply.github.com>
2019-08-26 10:24:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-function importScript(url){
-var req = new XMLHttpRequest();
-req.open('GET',url, false); 
-req.send(null);
-eval(req.responseText);
-}
+var notification = new Notification("Hi there!");
